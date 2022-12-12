64da1876144e78b395f109a803caacc3827009dc
Нурмухаммед Б. Бектурсунов <nbektursunov@megacom.kg>
2022-12-12 13:28:15 +0600

    fix: fixed validator

--- users/validators.py
+++ users/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_file_size(value):
     filesize = value.size
 
-    if filesize > 1000:
+    if filesize > 1024:
         raise ValidationError(
             "msg: The maximum file size that can be uploaded is 1MB"
         )
