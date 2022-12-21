0ea4e2bfd3d7f4a6e84a6bb91a975176a8514fb2
Нурмухаммед Б. Бектурсунов <nbektursunov@megacom.kg>
2022-12-21 16:51:55 +0600

    feat: added fiter by author nickname

--- core/settings.py
+++ core/settings.py
@@ -53,6 +53,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
     ),
--- posts/filters.py
+++ posts/filters.py
@@ -9,8 +9,8 @@ class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
 
 class PostFilterSet(django_filters.FilterSet):
     tag = CharInFilter(field_name="tag__name", lookup_expr="in")
-    author = django_filters.NumberFilter(field_name='author')
+    author = django_filters.CharFilter(field_name='author__nickname')
 
     class Meta:
         model = Post
-        fields = ("tag",)
+        fields = ("tag", "author")
