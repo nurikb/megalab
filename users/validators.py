fe614d94cf70e26ed46187df123b116cd1feebdd
Нурмухаммед Б. Бектурсунов <nbektursunov@megacom.kg>
2022-12-22 15:43:45 +0600

    changed image validator size

--- users/validators.py
+++ users/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def validate_file_size(value):
     filesize = value.size
-    if filesize > 5048576:
+    if filesize > 1048576:
         raise ValidationError(
             "msg: The maximum file size that can be uploaded is 1MB"
         )
